b6e604b09dda8dfdf1fa214212e456fd7248e06e
serena glyn lee-cultura <122268887+iamsegl@users.noreply.github.com>
2025-01-16 12:33:47 +0100

    fix: update parameter config file (#46)

--- atmos_validation/schemas/parameter_config.py
+++ atmos_validation/schemas/parameter_config.py
@@ -41,7 +41,7 @@ class ParameterConfig(BaseModel, arbitrary_types_allowed=True, extra=Extra.allow
     max: Union[float, int, Literal["NA"]]
     CF_standard_name: str
     dims: List[str]
-    qc_tests: Dict[str, QCTest] = Field(default_factory=list)
+    qc_tests: Dict[str, QCTest] = Field(default_factory=dict)
 
     @validator("dims")
     @classmethod
