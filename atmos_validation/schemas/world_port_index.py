17db40703a09c126d73aed63f56d088e69d3ac34
Deepthi Brundavanam <152874076+deepthi-equinor@users.noreply.github.com>
2025-08-07 07:22:00 +0000

    feat: add schema for wpi

---
+++ atmos_validation/schemas/world_port_index.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+class WorldPortIndex(BaseModel):
+    """World-wide maritime port information."""
+
+    fid: float
+    world_port_index_number: float
+    region_name: str
+    main_port_name: str
+    alternate_port_name: Optional[str] = Field(default=None)
+    unlocode: Optional[str] = Field(default=None)
+    country_code: str
+    world_water_body: str
+    tidal_range: float
+    entrance_width: float
+    channel_depth: float
+    anchorage_depth: float
+    cargo_pier_depth: float
+    oil_terminal_depth: float
+    liquified_natural_gas_terminal_depth: float
+    maximum_vessel_length: float
+    maximum_vessel_beam: float
+    maximum_vessel_draft: float
+    latitude: float
+    longitude: float
