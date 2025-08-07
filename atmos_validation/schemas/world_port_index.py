09922e1605b559a569c1e1a35280a60ab350bfd2
Deepthi Brundavanam <152874076+deepthi-equinor@users.noreply.github.com>
2025-08-07 09:40:08 +0000

    add validation for lat/lng

--- atmos_validation/schemas/world_port_index.py
+++ atmos_validation/schemas/world_port_index.py
@@ -1,12 +1,12 @@
 from typing import Optional
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, validator
 
 
 class WorldPortIndex(BaseModel):
     """World-wide maritime port information."""
 
-    id: float
+    oid: float
     world_port_index_number: float
     region_name: str
     main_port_name: str
@@ -26,3 +26,17 @@ class WorldPortIndex(BaseModel):
     maximum_vessel_draft: float
     latitude: float
     longitude: float
+
+    @validator("latitude")
+    def validate_latitude(cls, lat):
+        if not (-90 <= lat <= 90):
+            raise ValueError(f"Invalid latitude: {lat}. It must be between -90 and 90.")
+        return lat
+
+    @validator("longitude")
+    def validate_longitude(cls, lng):
+        if not (-180 <= lng <= 180):
+            raise ValueError(
+                f"Invalid longitude: {lng}. It must be between -180 and 180."
+            )
+        return lng
